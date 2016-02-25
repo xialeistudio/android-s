23380e9c16c155c6e53e47efd29870202e4ea341
xialeistudio <1065890063@qq.com>
2016-02-25 14:35:01 +0800

    circleProgressView

---
+++ testview/src/main/java/com/ddhigh/testview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ddhigh.testview;
+
+import android.app.DialogFragment;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnTest) {
+            ProgressFragment progressFragment = new ProgressFragment();
+            progressFragment.show(getFragmentManager(), "dialog");
+        }
+    }
+}
