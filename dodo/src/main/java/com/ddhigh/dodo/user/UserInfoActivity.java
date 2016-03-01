ed259257c4e47c8c485ef86e56fcdec569186d33
xialeistudio <1065890063@qq.com>
2016-03-01 12:17:49 +0800

    [dodo] activity返回

--- dodo/src/main/java/com/ddhigh/dodo/user/UserInfoActivity.java
+++ dodo/src/main/java/com/ddhigh/dodo/user/UserInfoActivity.java
@@ -2,7 +2,9 @@ package com.ddhigh.dodo.user;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.ddhigh.dodo.R;
 
@@ -21,5 +23,21 @@ public class UserInfoActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         x.view().inject(this);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
