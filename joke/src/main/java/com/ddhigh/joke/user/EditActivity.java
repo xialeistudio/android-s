327976a62f5935e6c1bb41712d1051494e8ec8e5
xialeistudio <1065890063@qq.com>
2016-03-05 17:30:26 +0800

    [joke] 编辑资料信息显示完成

--- joke/src/main/java/com/ddhigh/joke/user/EditActivity.java
+++ joke/src/main/java/com/ddhigh/joke/user/EditActivity.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.ddhigh.joke.MyApplication;
 import com.ddhigh.joke.R;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
 
 /**
@@ -19,6 +25,14 @@ import org.xutils.x;
  */
 @ContentView(R.layout.activity_user_edit)
 public class EditActivity extends AppCompatActivity {
+    MyApplication application;
+    @ViewInject(R.id.imageAvatar)
+    ImageView imageAvatar;
+    @ViewInject(R.id.txtNickname)
+    TextView txtNickname;
+    @ViewInject(R.id.txtEmail)
+    TextView txtEmail;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +41,19 @@ public class EditActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        application = (MyApplication) getApplication();
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        //显示用户信息
+        if (!TextUtils.isEmpty(application.user.getAvatar())) {
+            imageLoader.displayImage(application.user.getAvatar(), imageAvatar);
+        }
+        if (!TextUtils.isEmpty(application.user.getNickname())) {
+            txtNickname.setText(application.user.getNickname());
+        }
+        if (!TextUtils.isEmpty(application.user.getEmail())) {
+            txtEmail.setText(application.user.getEmail());
         }
     }
 
