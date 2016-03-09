5521f11e34b427aeab9f28a44b78b98ac4625137
xialeistudio <1065890063@qq.com>
2016-03-09 18:56:57 +0800

    上线

--- joke/src/main/java/com/ddhigh/joke/MyApplication.java
+++ joke/src/main/java/com/ddhigh/joke/MyApplication.java
@@ -38,7 +38,7 @@ public class MyApplication extends Application {
             HttpUtil.setToken(user.getToken());
         }
         //初始化请求类
-        HttpUtil.setApi("http://myjoke.duapp.com/v1");
+        HttpUtil.setApi("http://jokexx.duapp.com/v1");
         //初始化应用存储目录
         applicationPath = new File(Environment.getExternalStorageDirectory(), getPackageName());
         if (!applicationPath.isDirectory()) {
