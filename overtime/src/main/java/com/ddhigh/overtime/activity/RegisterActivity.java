aa2ad0ecc5e63dde1a0b1817fdc71f2e7a47b8f5
xialeistudio <1065890063@qq.com>
2016-03-11 00:49:56 +0800

    [overtime]登录&&注册表单

--- overtime/src/main/java/com/ddhigh/overtime/activity/RegisterActivity.java
+++ overtime/src/main/java/com/ddhigh/overtime/activity/RegisterActivity.java
@@ -1,10 +1,9 @@
 package com.ddhigh.overtime.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import com.ddhigh.overtime.R;
@@ -12,27 +11,22 @@ import com.ddhigh.overtime.R;
 import org.xutils.view.annotation.ContentView;
 import org.xutils.x;
 
-@ContentView(R.layout.activity_login)
-public class LoginActivity extends AppCompatActivity {
+@ContentView(R.layout.activity_register)
+public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         x.view().inject(this);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.menu_register, menu);
-        return true;
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+            actionBar.setDisplayHomeAsUpEnabled(true);
     }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.menuRegister) {
-            Intent intent = new Intent(this, RegisterActivity.class);
-            startActivity(intent);
+        if (item.getItemId() == android.R.id.home) {
+            finish();
             return true;
         }
         return super.onOptionsItemSelected(item);
