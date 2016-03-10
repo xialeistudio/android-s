278f0d0b23b3942f3af11b088be088286bad0049
xialeistudio <1065890063@qq.com>
2016-03-10 23:16:35 +0800

    Model equal OK

--- overtime/src/test/java/com/ddhigh/overtime/model/DepartmentTest.java
+++ overtime/src/test/java/com/ddhigh/overtime/model/DepartmentTest.java
@@ -1,7 +1,5 @@
 package com.ddhigh.overtime.model;
 
-import org.json.JSONObject;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -13,30 +11,17 @@ import static org.junit.Assert.assertEquals;
  * @date 2016/3/10 0010
  */
 public class DepartmentTest {
-    private Department department;
 
-    @Before
-    public void setUp() throws Exception {
-        department = new Department();
+    @Test
+    public void testEquals() throws Exception {
+        Department department = new Department();
         department.setDepartment_id(1);
         department.setName("技术部");
-    }
 
-    @Test
-    public void testDecode() throws Exception {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("department_id", 1);
-        jsonObject.put("name", "技术部");
-        Department department1 = new Department();
-        department1.decode(jsonObject);
-        assertEquals(department, department1);
-    }
+        Department department2 = new Department();
+        department2.setDepartment_id(1);
+        department2.setName("技术部");
 
-    @Test
-    public void testEncode() throws Exception {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("department_id", 1);
-        jsonObject.put("name", "技术部");
-        assertEquals(jsonObject.toString(), department.encode().toString());
+        assertEquals(department,department2);
     }
 }
